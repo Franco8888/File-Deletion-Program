4663c231040ae5ba99273a25ca58976812eba2c9
Franco Smit <francoissmt@gmail.com>
2021-09-13 22:45:34 +0200

    Added deletion class without deleting from children folders

---
+++ FileDeletion/FileDeletion/DeleteExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FileDeletion
+{
+    public class DeleteExtension
+    {
+        public string Path { get; private set; }
+
+        public List<string> Extensions { get; private set; }
+
+        public DeleteExtension(string folderpath, List<string> ex )
+        {
+            Path = folderpath;
+            Extensions = ex;
+        }
+
+        public void DeleteFiles()
+        {
+            List<FileInfo> list = new List<FileInfo>();
+            foreach (string ext in Extensions)
+                list.AddRange(new DirectoryInfo(Path).GetFiles("*" + ext).Where(p =>
+                      p.Extension.Equals(ext, StringComparison.CurrentCultureIgnoreCase))
+                      .ToArray());
+
+            foreach (var file in list)
+            {
+                File.Delete(file.FullName);
+            }
+        }
+
+    }
+}
---
+++ FileDeletion/FileDeletion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileDeletion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A Folder Path:");
+
+            var inputPath = Console.ReadLine();
+
+            List<string> extensions = new();
+
+            Console.WriteLine("Enter extensions, each seperated by an enter, press 1 to exit");
+
+            while(true)
+            {
+                var extension = Console.ReadLine();
+                if(extension == "1" )
+                {
+                    break;
+                }
+                extensions.Add(extension);
+            }
+
+            DeleteExtension delExt = new(inputPath, extensions);
+
+            delExt.DeleteFiles();
+        }
+    }
+}
